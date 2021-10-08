e99e7044ed2a7112e0c1a4018758da229831cd3e
qnote Adwin <ying@qnote.co.jp>
2021-10-08 17:49:11 +0900

    [neovim] Enable all treesitter modules

--- neovim/.config/nvim/lua/configs/treesitter.lua
+++ neovim/.config/nvim/lua/configs/treesitter.lua
@@ -22,4 +22,18 @@ ts_config.setup {
   highlight = {
     enable = true,
   },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection    = "gnn",
+      node_incremental  = "grn",
+      scope_incremental = "grc",
+      node_decremental  = "grm",
+    },
+  },
+
+  indent = {
+    enable = true,
+  },
 }
