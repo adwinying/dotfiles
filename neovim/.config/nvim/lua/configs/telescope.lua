20e7fe086f78f96393b8719614b35ed04154cec6
qnote Adwin <ying@qnote.co.jp>
2021-10-08 22:34:44 +0900

    [neovim] Add fuzzy search for telescope

--- neovim/.config/nvim/lua/configs/telescope.lua
+++ neovim/.config/nvim/lua/configs/telescope.lua
@@ -24,7 +24,7 @@ telescope.setup {
   extensions = {
     fzf = {
       fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = false, -- override the generic sorter
+      override_generic_sorter = true, -- override the generic sorter
       override_file_sorter = true, -- override the file sorter
       case_mode = "smart_case", -- or "ignore_case" or "respect_case"
       -- the default case_mode is "smart_case"
@@ -33,6 +33,13 @@ telescope.setup {
     media_files = {
       filetypes = { "png", "webp", "jpg", "jpeg" },
       find_cmd = "rg",
+    },
+  },
+
+  pickers = {
+    grep_string = {
+      search = "",
+      only_sort_text = false,
     },
   },
 }
