0975b701401640008f93cfa330421f97d81c83f5
Adwin Ying <adwinying@gmail.com>
2021-10-10 17:15:45 +0900

    [neovim] Configure luasnip jump shortcut

--- neovim/.config/nvim/lua/configs/cmp.lua
+++ neovim/.config/nvim/lua/configs/cmp.lua
@@ -26,16 +26,39 @@ cmp.setup {
   },
 
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    ['<C-e>'] = cmp.mapping.close(),
+    ["<C-p>"]     = cmp.mapping.select_prev_item(),
+    ["<C-n>"]     = cmp.mapping.select_next_item(),
+    ["<C-u>"]     = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"]     = cmp.mapping.scroll_docs(4),
+    ['<C-e>']     = cmp.mapping.close(),
     ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
+    ["<CR>"]      = cmp.mapping.confirm(),
+
+    ["<C-j>"] = function(fallback)
+      if require("luasnip").expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes(
+          "<Plug>luasnip-expand-or-jump",
+          true,
+          true,
+          true
+        ), "")
+      else
+        fallback()
+      end
+    end,
+
+    ["<C-k>"] = function(fallback)
+      if require("luasnip").jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes(
+          "<Plug>luasnip-jump-prev",
+          true,
+          true,
+          true
+        ), "")
+      else
+        fallback()
+      end
+    end,
   },
 
   sources = {
