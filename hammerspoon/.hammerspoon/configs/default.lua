f129b570d214a2f04a1bd43753de523b7333c5a5
Adwin Ying <adwinying@gmail.com>
2021-10-10 12:14:27 +0900

    [hs] Init hhtwm config

--- hammerspoon/.hammerspoon/configs/default.lua
+++ hammerspoon/.hammerspoon/configs/default.lua
@@ -18,6 +18,34 @@ default_config.keybindings = {
 }
 
 -- =============================================================================
+-- Window manager
+-- =============================================================================
+default_config.wm = {
+  -- menubar offset
+  menubar_offset = 25,
+
+  -- gap size
+  gap_size = 9,
+
+  -- available layouts
+  enabled_layouts = {
+    "main-left",
+    "monocle",
+    "floating",
+    "main-center",
+  },
+
+  -- default layout
+  default_layout = "monocle",
+
+  -- window rules
+  rules = {
+    -- don't tile Hammerspoon Console
+    { app = 'Hammerspoon', title = 'Hammerspoon Console', tile = false },
+  },
+}
+
+-- =============================================================================
 -- Paths
 -- =============================================================================
 
--- hammerspoon/.hammerspoon/modules/wm.lua
+++ hammerspoon/.hammerspoon/modules/wm.lua
@@ -3,12 +3,37 @@
 -- Window manager related config
 --
 
+local config = require("configs").wm
+
 -- =============================================================================
 -- Initialization
 -- =============================================================================
 
+-- disble window animations
+hs.window.animationDuration = 0
+
 -- initialize as global object so it's not garbage collected
 Hhtwm = require("libraries.hhtwm")
+
+-- set gap sizes
+Hhtwm.margin = config.gap_size * 2
+
+-- set screen margins
+Hhtwm.screenMargin = {
+  top    = config.menubar_offset + config.gap_size,
+  bottom = config.gap_size,
+  left   = config.gap_size,
+  right  = config.gap_size,
+}
+
+-- set enabled layouts
+Hhtwm.enabledLayouts = config.enabled_layouts
+
+-- set default layout
+Hhtwm.defaultLayout = config.default_layout
+
+-- filters (window rules)
+Hhtwm.filters = config.rules
 
 -- start hhtwm
 Hhtwm.start()
