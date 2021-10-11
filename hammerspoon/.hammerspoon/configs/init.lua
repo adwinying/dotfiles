3372417169b03276c74bb84e185017d433bdd741
qnote Adwin <ying@qnote.co.jp>
2021-10-11 12:30:33 +0900

    [hs] Fix merge configs not working

--- hammerspoon/.hammerspoon/configs/init.lua
+++ hammerspoon/.hammerspoon/configs/init.lua
@@ -11,7 +11,7 @@ local config = require("configs.default")
 local custom = require("configs.custom")
 
 -- merge configs
-local extend_table = function (...)
+local function extend_table (...)
   local ret = {}
 
   for i = 1, select('#', ...) do
@@ -20,7 +20,7 @@ local extend_table = function (...)
     for k, v in pairs(tbl) do
       if type(v) == "table" then
         if type(ret[k] or false) == "table" then
-          extend_table(ret[k] or {}, tbl[k] or {})
+          ret[k] = extend_table(ret[k] or {}, tbl[k] or {})
         else
           ret[k] = v
         end
