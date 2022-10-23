3bb35cfa6ce5020f6bdb33311d521689f52f849d
Adwin Ying <adwinying@gmail.com>
2022-10-23 16:20:00 +0900

    [neovim] Update configs to support v0.8

--- neovim/.config/nvim/lua/configs/lspconfig.lua
+++ neovim/.config/nvim/lua/configs/lspconfig.lua
@@ -108,27 +108,6 @@ local custom_configs = {
     return config
   end,
 
-  eslint = function (config)
-    config.filetypes = {
-      "javascript", "javascriptreact", "javascript.jsx",
-      "typescript", "typescriptreact", "typescript.tsx",
-      "vue",
-    }
-
-    config.on_attach = function (client, bufnr)
-      -- neovim's LSP client does not currently support dynamic capabilities registration, so we need to set
-      -- the resolved capabilities of the eslint server ourselves!
-      client.resolved_capabilities.document_formatting = true
-      on_attach(client, bufnr)
-    end
-
-    config.settings = {
-      format = { enable = true }, -- this will enable formatting
-    }
-
-    return config
-  end,
-
   phpactor = function (config)
     config.init_options = {
         ["language_server_phpstan.enabled"] = true,
@@ -141,9 +120,8 @@ local custom_configs = {
 
 -- make config for a server
 local make_config = function (server)
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  local capabilities = require("cmp_nvim_lsp").default_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
-  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
   local config = {
     -- enable snippet support
