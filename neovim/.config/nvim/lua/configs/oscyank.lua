83519fec4feaff9384203b81b422ba1ffb4e785c
Adwin Ying <adwinying@gmail.com>
2023-02-27 19:06:14 +0900

    [neovim] Combine external interaction plugins/configs into a single file

--- neovim/.config/nvim/lua/configs/oscyank.lua
+++
@@ -1,4 +0,0 @@
--- copy contents of + register to clipboard
-vim.cmd([[
-  autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif
-]])
---
+++ neovim/.config/nvim/lua/plugins/external.lua
@@ -0,0 +1,67 @@
+return {
+  -- tmux integration
+  {
+    "aserowy/tmux.nvim",
+    event = "VeryLazy",
+    config = {
+      copy_sync = {
+        -- enables copy sync and overwrites all register actions to
+        -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
+        enable = false,
+
+        -- TMUX >= 3.2: yanks (and deletes) will get redirected to system
+        -- clipboard by tmux
+        redirect_to_clipboard = false,
+
+        -- offset controls where register sync starts
+        -- e.g. offset 2 lets registers 0 and 1 untouched
+        register_offset = 0,
+
+        -- sync clipboard overwrites vim.g.clipboard to handle * and +
+        -- registers. If you sync your system clipboard without tmux, disable
+        -- this option!
+        sync_clipboard = true,
+
+        -- syncs deletes with tmux clipboard as well, it is adviced to
+        -- do so. Nvim does not allow syncing registers 0 and 1 without
+        -- overwriting the unnamed register. Thus, ddp would not be possible.
+        sync_deletes = true,
+      },
+
+      navigation = {
+        -- cycles to opposite pane while navigating into the border
+        cycle_navigation = true,
+
+        -- enables default keybindings (C-hjkl) for normal mode
+        enable_default_keybindings = true,
+
+        -- prevents unzoom tmux when navigating beyond vim border
+        persist_zoom = false,
+      },
+
+      resize = {
+        -- enables default keybindings (A-hjkl) for normal mode
+        enable_default_keybindings = true,
+
+        -- sets resize steps for x axis
+        resize_step_x = 5,
+
+        -- sets resize steps for y axis
+        resize_step_y = 5,
+      }
+    },
+  },
+
+  -- OSC52 (universal clipboard) integration
+  {
+    "ojroques/vim-oscyank",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      -- copy contents of + register to clipboard
+      vim.cmd([[
+autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif
+      ]])
+    end,
+  },
+
+}
