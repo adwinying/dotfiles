fc1af6109a122d5bb6977cb7830898ec78c4cccd
Adwin Ying <adwinying@gmail.com>
2023-04-09 22:40:02 +0900

    [neovim] Remove OSC52 plugin

--- neovim/.config/nvim/lua/plugins/external.lua
+++ neovim/.config/nvim/lua/plugins/external.lua
@@ -51,20 +51,4 @@ return {
       }
     },
   },
-
-  -- OSC52 (universal clipboard) integration
-  {
-    "ojroques/nvim-osc52",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      -- copy contents of + register to clipboard
-      vim.api.nvim_create_autocmd('TextYankPost', {
-        callback = function ()
-          if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
-            require('osc52').copy_register('+')
-          end
-        end
-      })
-    end,
-  },
 }
