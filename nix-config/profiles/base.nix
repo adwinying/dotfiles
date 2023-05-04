89324d867d46f4fc8ca8d9cd753eaaf69bbf0552
Adwin Ying <adwinying@gmail.com>
2023-05-04 16:59:26 +0900

    [nix] Add session vars to user config

--- nix-config/profiles/base.nix
+++ nix-config/profiles/base.nix
@@ -36,6 +36,13 @@
   home = {
     inherit username;
     homeDirectory = "/home/${username}";
+    sessionVariables = {
+      LANG = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      EDITOR = "nvim";
+      PAGER = "less -FirSwX";
+    };
   };
 
   # Enable home-manager
