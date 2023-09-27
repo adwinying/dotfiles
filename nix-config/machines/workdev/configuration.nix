d28e18ecf1d59f224c5234379c5a75e3c388b098
Adwin Ying <adwinying@gmail.com>
2023-09-27 11:57:13 +0900

    [nix] [workdev] Remove unused configs

--- nix-config/machines/workdev/configuration.nix
+++ nix-config/machines/workdev/configuration.nix
@@ -15,15 +15,8 @@
   # Import overlays for this machine
   nixpkgs.overlays = [];
 
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-
   # No need firewall as this machine is not public facing
   networking.firewall.enable = false;
-
-  # # Enable QEMU's guest agent
-  # services.qemuGuest.enable = true;
-  # services.spice-vdagentd.enable = true;
 
   # Import addtional user profiles for this machine
   _module.args.profiles = [
