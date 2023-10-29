30465836b941fdd635cc0eed55f5f6a3532a5e4c
Adwin Ying <adwinying@gmail.com>
2023-10-29 19:39:18 +0900

    [nix] [tunnel] Fix plex remote access by adding reverse proxy

--- nix-config/machines/tunnel/configuration.nix
+++ nix-config/machines/tunnel/configuration.nix
@@ -2,7 +2,7 @@
 # tunnel-specific configs
 #
 
-{ ... }: {
+{ username, ... }: {
   # Import system modules for this machine
   imports = [
     ./hardware-configuration.nix
@@ -19,6 +19,38 @@
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.device = "nodev";
   boot.initrd.kernelModules = [ "nvme" ];
+
+  # Let's Encrypt
+  security.acme = {
+    acceptTerms = true;
+    certs = {
+      "iadw.in" = {
+        domain = "*.iadw.in";
+        email = "admin@iadw.in";
+        group = "acme";
+        dnsProvider = "cloudflare";
+        dnsResolver = "1.1.1.1:53";
+        credentialsFile = "/home/${username}/.secrets/acme.env";
+      };
+    };
+  };
+  users.users.${username}.extraGroups = [ "acme" ];
+
+  # Configure caddy
+  services.caddy = {
+    enable = true;
+    virtualHosts = let
+      common = {
+        useACMEHost = "iadw.in";
+      };
+    in {
+      plex = common // {
+        hostName = "plex.iadw.in";
+        extraConfig = "reverse_proxy localhost:32400";
+      };
+    };
+  };
+  users.users.caddy.extraGroups = [ "acme" ];
 
   # Configure firewall
   networking.firewall = {
