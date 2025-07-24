2f3457a05a100f573195be4ed3550f47966c7dbf
Adwin Ying <adwinying@gmail.com>
2025-07-24 11:25:15 +0900

    [nvim] Show phpstan errors with nvim-lint

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -853,6 +853,39 @@ require("lazy").setup({
     end,
   },
 
+  -- Linting
+  {
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      -- Auto install & manage LSP servers
+      { 'mason-org/mason.nvim', config = true },
+      { 'rshkarin/mason-nvim-lint', config = true },
+    },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint' },
+        php = { 'phpstan' },
+      }
+
+      -- Create autocommand which carries out the actual linting
+      -- on the specified events.
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          -- Only run the linter in buffers that you can modify in order to
+          -- avoid superfluous noise, notably within the handy LSP pop-ups that
+          -- describe the hovered symbol using Markdown.
+          if vim.bo.modifiable then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+
   -- Completion Window
   {
     "saghen/blink.cmp",
