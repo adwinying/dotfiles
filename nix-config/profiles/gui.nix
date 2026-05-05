a2d2963fcfeff613a1938310211e581620cdf6ab
Adwin Ying <adwinying@gmail.com>
2026-05-05 19:41:27 +0900

    [nix] Update outdated package name

--- nix-config/profiles/gui.nix
+++ nix-config/profiles/gui.nix
@@ -32,7 +32,7 @@
     home.packages = with pkgs; [
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       hackgen-font
       hackgen-nf-font
     ];
